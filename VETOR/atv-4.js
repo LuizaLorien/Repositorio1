1638466de8cbd292c3938e52c5332d510d90930d
luiza <luiza.pereira@aluno.al.sesi.com.br>
2023-09-05 08:29:40 -0300

    ola

--- VETOR/atv-4.js
+++ VETOR/atv-4.js
@@ -1,1 +1,21 @@
 //crie um programa que receba os salarios dos funcionarios e armazene em um array. depois esclua o menor e o maior salario e mostre a média.
+
+let salario;
+let salarios = [];
+while (true) {
+  salario = Number(prompt("Digite o salário do funcionário: "));
+
+  if (salario == "") {
+    break;
+  }
+}
+salarios.append(float(salario));
+
+// Exclua o menor e o maior salário.
+salarios.sort();
+salarios.pop(0);
+salarios.pop(-1);
+
+//Mostre a média.
+media = sum(salarios) / len(salarios);
+document.write("A média dos salários é:", media);
---
+++ VETOR/exer-4.js
@@ -0,0 +1,11 @@
+let vetor, vetor2, num
+vetor = [1,3,5,7]
+vetor2 = [2,4,6,8]
+
+for(let i=vetor; i=4; i++){
+    vetor= Number(prompt("escreva um número"))
+
+}for(i=vetor2; i=4;i++){
+    vetor= Number(prompt("escreva um número"))
+
+}
