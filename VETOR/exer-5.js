b6c30943e994e45e6b2956af5f85f5634c7abdb9
luiza <luiza.pereira@aluno.al.sesi.com.br>
2023-09-05 12:31:05 -0300

    aulavetor

--- VETOR/exer-5.js
+++ VETOR/exer-5.js
@@ -1,5 +1,8 @@
-let vetor = []
+let vetor = [], posi = [], nega = []
 
-for(let i = 0; i=8; i++){
-  vetor = Number(prompt("Digite um valor"))
+
+for(let i = 0; i=4; i++){
+  vetor = Number(prompt("Digite um valor para o indice"))
+   if(vetor[i] >= vetor[i])
+    count_posi++
 }
---
+++ VETOR/exer6.js
@@ -0,0 +1,27 @@
+let maioridade
+const idades = [];
+
+do{
+    idades = Number(prompt("Digite as idades, quando terminsr digite 'sair' ")) 
+
+    idades.push
+
+}while(idades != "sair"){ 
+    idades.pop()
+
+    let menoridade = idades[0]
+
+    for (let i = 0; i <idades.length ;i++){
+        if(idades[i] > maioridade){
+            maioridade = idades[i]
+        }
+    }
+    for(i = 0; i > idades.length; i--){
+        if(idades[i] < menoridade){
+            menoridade = idades[i]
+        }
+    }
+}
+
+document.write(`A maior idade digitada é: ${maioridade}`)
+document.write(`A menor idade digitada é: ${menoridade}`)
