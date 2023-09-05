a97003fe59d78458141d378cbc9debb60533b636
luiza <luiza.pereira@aluno.al.sesi.com.br>
2023-09-05 09:15:39 -0300

    vet

--- VETOR/exer-4.js
+++ VETOR/exer-4.js
@@ -1,11 +1,23 @@
-let vetor, vetor2, num
-vetor = [1,3,5,7]
-vetor2 = [2,4,6,8]
+let vetor = [],
+  vetor2 = [],
+  vetor3 = [];
+let j = 0;
 
-for(let i=vetor; i=4; i++){
-    vetor= Number(prompt("escreva um número"))
+for (let i = 0; i < 3; i++) {
+  vetor[i] = Number(prompt(`escreva um valor para o indice 1` + i + `:`));
+  vetor3[i] = vetor[i];
+  j++;
 
-}for(i=vetor2; i=4;i++){
-    vetor= Number(prompt("escreva um número"))
-
+  vetor2[i] = Number(prompt(`digite um valor para o indice 2`));
+  vetor3[j] = vetor2[i];
+  j++;
 }
+document.write(`vetor 1 ${vetor}<br>`);
+document.write(`vetor 2 ${vetor2}<br>`);
+
+for (let i = 0; i < vetor3.length; i++) {
+  console.log(vetor3[i]);
+}
+document.write(`vetor 3 ${vetor}<br>`);
+
+//mostrar
---
+++ VETOR/exer-5.js
@@ -0,0 +1,5 @@
+let vetor = []
+
+for(let i = 0; i=8; i++){
+  vetor = Number(prompt("Digite um valor"))
+}
